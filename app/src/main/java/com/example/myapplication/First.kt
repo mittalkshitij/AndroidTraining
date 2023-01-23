68cf39ddac21087a4642c1db3043bb718096c26b
kshitijmittal <mittalkshitij20@gmail.com>
2023-01-23 11:12:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/First.kt
@@ -0,0 +1,62 @@
+package com.example.myapplication
+
+import java.util.Scanner
+
+fun main(){
+
+/*
+        println("Hello, world!!!")
+
+        var name="ansari"
+        var college: String= "abc"
+
+        name="xyz"
+
+        val id=123
+        println("my name is "+name)
+        println("my name is $name and college is $college")
+        println("length of ${name.length}")
+
+        var age: Int=43
+        var newage : Double = age.toDouble()
+
+        when(age){
+
+                25-> println("young")
+                30,31,32 -> {
+                        println("mid age")
+                        println("nice age")
+                }
+
+                in 40..50 -> {
+                        println("old age")
+                }
+
+                else -> println("no age entered")
+        }*/
+
+/*        var arr= arrayOf(2,3,4,5,6,7)
+
+        for(i in arr)
+        {
+                println(i)
+        }
+
+        for(i in 0..10)
+        {
+                println(i)
+        }
+
+        for(i in 10 downTo 1 step 2)
+        {
+                println(i)
+        }*/
+
+
+
+
+
+
+
+
+}
